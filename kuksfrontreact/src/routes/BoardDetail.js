eb481f83b70ac185be5753a551cfc0a415398388
Kukvly <kukvly96@gmail.com>
2024-04-22 16:08:26 +0900

    routes func setting

---
+++ kuksfrontreact/src/routes/BoardDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BoardDetail = () => {
+    return (
+        <div>
+            게시판 상세보기.
+        </div>
+    );
+};
+
+export default BoardDetail;
--- kuksfrontreact/src/routes/BoardList.js
+++ kuksfrontreact/src/routes/BoardList.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 const BoardList = () => {
+    const [boardList, setBoardList] = useState([]);
+
     const getBoardList = async () => {
         const res = (await axios.get('//localhost:8081/board')).data
+        setBoardList(res.data);
         console.log(res.data)
+        
+        const pngn = res.pagination;
+        console.log(pngn);
     }
+
+    useEffect(() => {
+        // 게시글 목록 조회 함수 호출
+        getBoardList();
+    }, []);
+
     return(
         <div>
-            게시판 목록 출력
+            <ul>
+                {boardList.map((board) => (
+                    // 게시판 목록 출력            
+                <li key={board.idx}>
+                    <Link to={`/board/${board.idx}`}>{board.title}</Link>
+                </li>
+                ))}
+            </ul>
         </div>
     );
 };
