8249163d7ca04c4ba64ef7b288ca13c31b90eb1f
Kukvly <kukvly96@gmail.com>
2024-04-29 16:10:54 +0900

    test2"

--- kuksfrontreact/src/profile/KuksProfile.js
+++ kuksfrontreact/src/profile/KuksProfile.js
@@ -1,7 +1,16 @@
-const KuksProfile = () => {
+import React, {useEffect} from 'react';
 
-    alert("Write Profile ");
-}
+const KuksProfile = () => {
+    useEffect(() => {
+        alert("Kuks profile");
+    }, []);
+
+    return(
+        <div>
+            <b>정상적으로 들어왔다. 이제 수정해야 될 페이지다.</b>
+        </div>
+    );
+};
 
 
 export default KuksProfile;
